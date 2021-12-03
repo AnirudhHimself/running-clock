92559267aa047c4b78369e5a2e5364e1a98099ae
Anirudh <17210213+AnirudhHimself@users.noreply.github.com>
2021-12-02 21:04:16 -0600

    optimize fonts

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -3,7 +3,7 @@ import "./Clock.scss";
 
 export const Clock = (props: any) => {
   return (
-    <div aria-label={props.ariaLabel}>
+    <div aria-role="timer" aria-label={props.ariaLabel}>
       <Digit value={props.value[0]} />
       <Digit value={props.value[1]} />
       <span className="clock-divider" aria-hidden={"true"}>
