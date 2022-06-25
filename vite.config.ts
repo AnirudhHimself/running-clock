28bc5e5670478b6f915b35cc9ff01e124fbe301f
AnirudhHimself <17210213+AnirudhHimself@users.noreply.github.com>
2022-06-25 17:29:38 -0500

    update hashing strategy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          if (assetInfo.name.includes('css')) {
+            return '[name].[hash].[ext]';
+          }
+          return '[name].[ext]';
+        },
+        entryFileNames: `[name].[hash].js`,
+      },
+    },
+  },
   plugins: [svelte()],
 });
